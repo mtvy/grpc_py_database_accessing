6784d983e5019d46d3ecf7b777319fe34d6429d2
Mtvy <m.d.prudnik@gmail.com>
2023-01-16 05:24:00 +0300

    ADD DB HOST TO CLASS

--- api/grpc/__init__.py
+++ api/grpc/__init__.py
@@ -1,3 +1,3 @@
 
-from api.grpc.database_pb2 import GetDbRequest, GetDbResponse, InsertDbRequest, InsertDbResponse, DeleteDbRequest, DeleteDbResponse
-from api.grpc.database_pb2_grpc import DatabaseServicer, DatabaseStub, add_DatabaseServicer_to_server
+from grpc_py_database_accessing.api.grpc.database_pb2 import GetDbRequest, GetDbResponse, InsertDbRequest, InsertDbResponse, DeleteDbRequest, DeleteDbResponse
+from grpc_py_database_accessing.api.grpc.database_pb2_grpc import DatabaseServicer, DatabaseStub, add_DatabaseServicer_to_server
--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 
 import grpc, sys
 
-import api.grpc as api
+import grpc_py_database_accessing.api.grpc as api
 
 
 TB = 'qrcodes_tb'
@@ -9,22 +9,23 @@ DB_HOST = 'postgres://postgres:postgres@postgres:5433/postgres'
 
 class Database:
 
-    def __init__(self, host: str) -> None:
+    def __init__(self, host: str, dbHost: str) -> None:
         self.host = host
         self.conn = grpc.insecure_channel(host) 
         self.stub = api.DatabaseStub(self.conn)
+        self.dbHost = dbHost
     
-    def get(self, columns: str, table: str, condition: str, db_host: str) -> api.GetDbResponse:
+    def get(self, columns: str, table: str, condition: str) -> api.GetDbResponse:
         return self.stub.GetDb(api.GetDbRequest(
-            columns=columns, table=table, condition=condition, db_host=db_host))
+            columns=columns, table=table, condition=condition, db_host=self.dbHost))
 
-    def insert(self, table: str, columns: str, values: str, db_host: str) -> api.InsertDbResponse:
+    def insert(self, table: str, columns: str, values: str) -> api.InsertDbResponse:
         return self.stub.InsertDb(api.InsertDbRequest(
-            table=table, columns=columns, values=values, db_host=db_host))
+            table=table, columns=columns, values=values, db_host=self.dbHost))
 
-    def delete(self, table : str, condition : str, db_host : str) -> api.DeleteDbResponse:
+    def delete(self, table : str, condition : str) -> api.DeleteDbResponse:
         return self.stub.DeleteDb(api.DeleteDbRequest(
-            table=table, condition=condition, db_host=db_host))
+            table=table, condition=condition, db_host=self.dbHost))
     
 
 def run(host):
